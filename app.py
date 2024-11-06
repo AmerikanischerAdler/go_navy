5a2d26123fb1201f879c9c1ddd79e0de44f06a0f
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-11-05 22:42:00 -0500

    added links for history branch

--- app.py
+++ app.py
@@ -5,18 +5,58 @@ from flask import Flask, render_template, request, redirect, session, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')
 
+# History
 @app.route('/history')
 def gallery():
     return render_template('history.html')
 
+@app.route('/interests')
+def interests():
+    return render_template('interests.html')
+
+@app.route('/aspirations')
+def aspirations():
+    return render_template('aspirations.html')
+
+@app.route('/plans')
+def plans():
+    return render_template('plans.html')
+
+@app.route('/father')
+def father():
+    return render_template('father.html')
+
+@app.route('/ggpa')
+def ggpa():
+    return render_template('ggpa.html')
+
+@app.route('/sarlo')
+def sarlo():
+    return render_template('sarlo.html')
+
+@app.route('/kindergarten')
+def kindergarten():
+    return render_template('kindergarten.html')
+
+@app.route('/seals')
+def seals():
+    return render_template('seals.html')
+
+@app.route('/nass')
+def nass():
+    return render_template('nass.html')
+
+# Student
 @app.route('/student')
 def profile():
     return render_template('nass.html')
 
+# Career
 @app.route('/career')
 def sign():
     return render_template('app.html')
